3830034f0f592f688fe8957137ac5efe41ed4b01
Reawpai Chunsoi <reawpai.chun@yahoo.com>
2020-09-08 23:39:22 +0700

    Initial Template

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-backgrounds/register',
+    '@storybook/addon-storysource',
+  ],
+}
